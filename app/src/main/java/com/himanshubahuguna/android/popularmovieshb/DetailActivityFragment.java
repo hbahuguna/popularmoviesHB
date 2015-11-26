17cfbd39d1471aee52d0f1758bcdad93ffedbc4c
Himanshu <hbahuguna@about.com>
2015-11-25 20:45:27 -0500

    popular movies stage 1

---
+++ app/src/main/java/com/himanshubahuguna/android/popularmovieshb/DetailActivityFragment.java
@@ -0,0 +1,22 @@
+package com.himanshubahuguna.android.popularmovieshb;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hbahuguna on 11/24/2015.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
