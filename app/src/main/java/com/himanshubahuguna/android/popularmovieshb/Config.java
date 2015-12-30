1f02399ecc0756b053373f2143db9414a157251e
Himanshu <hbahuguna@about.com>
2015-12-30 06:50:25 -0500

    added settingsactivity

--- app/src/main/java/com/himanshubahuguna/android/popularmovieshb/Config.java
+++ app/src/main/java/com/himanshubahuguna/android/popularmovieshb/Config.java
@@ -4,7 +4,7 @@ package com.himanshubahuguna.android.popularmovieshb;
  * Created by hbahuguna on 12/20/2015.
  */
 public class Config {
-    public static final String API_KEY = "api_key";
+    public static final String API_KEY = "c4b2831b4d3c932c6cde4ac5eae5d616";
     public static final String API_BASE_URL = "http://api.themoviedb.org/3/";
     public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
     public static final String YOUTUBE_TRAILER_URL = "https://www.youtube.com/watch?v=";
